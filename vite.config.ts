e9e78aa403bbee7a1170e62ac0d338cdf625326a
zeroxbob <zeroxbob@protonmail.com>
2025-07-25 13:43:14 +0200

    Deploy with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: process.env.NODE_ENV === 'production' ? '/nostrpow/' : '/',
   server: {
     host: "::",
     port: 8080,
